9dc322a7d71c69930bfef2316c689d63e0cf99ad
Nashir Janmohamed <nashirbass@gmail.com>
2020-06-01 20:28:31 -0700

    Implement sieve of atkin

--- rsa_vis/src/main.rs
+++ rsa_vis/src/main.rs
@@ -5,10 +5,18 @@ mod rsa;
 fn main() {
     println!("Hello, world!");
     test_sieve_of_eratosthenes(100);
+    test_sieve_of_atkin(100);
 }
 
 fn test_sieve_of_eratosthenes(n: usize) {
     let l = primes::sieve_of_eratosthenes(n);
+    for i in l {
+        println!("{}", i);
+    }
+}
+
+fn test_sieve_of_atkin(n: usize) {
+    let l = primes::sieve_of_atkin(n);
     for i in l {
         println!("{}", i);
     }
--- rsa_vis/src/primes.rs
+++ rsa_vis/src/primes.rs
@@ -25,3 +25,80 @@ pub fn sieve_of_eratosthenes(n: usize) -> std::vec::Vec<u32> {
     }
     return res;
 }
+
+/// Return a list of prime numbers in the range of [2,n]
+///
+/// # Arguments
+///
+/// * `n` - A usize specifying range in which to generate primes
+pub fn sieve_of_atkin(limit: usize) -> std::vec::Vec<u32> {
+    let mut res = Vec::new();
+    // 2 and 3 are known to be prime
+    if limit > 2 {
+        res.push(2);
+    }
+    if limit > 3 {
+        res.push(3);
+    }
+
+    // Initialise the sieve array with false values
+    let mut sieve = vec![false; limit];
+
+    /* Mark sieve[n] is true if one
+       of the following is true:
+    a) n = (4*x*x)+(y*y) has odd number of
+       solutions, i.e., there exist
+       odd number of distinct pairs (x, y)
+       that satisfy the equation and
+        n % 12 = 1 or n % 12 = 5.
+    b) n = (3*x*x)+(y*y) has odd number of
+       solutions and n % 12 = 7
+    c) n = (3*x*x)-(y*y) has odd number of
+       solutions, x > y and n % 12 = 11 */
+    let mut x:i32 = 1;
+    let mut limit32:i32 = limit as i32;
+    while x*x < limit32 {
+        let mut y = 1;
+        while y*y < limit32 {
+            // Main part of Sieve of Atkin
+            let mut n:i32 = (4 * x * x) + (y * y);
+            if n <= limit32 && (n % 12 == 1 || n % 12 == 5) {
+                sieve[n as usize] ^= true;
+            }
+
+            n = (3 * x * x) + (y * y);
+            if n <= limit32 && n % 12 == 7 {
+                sieve[n as usize] ^= true;
+            }
+
+            n = (3 * x * x) - (y * y);
+            if x > y && n <= limit32 && n % 12 == 11 {
+                sieve[n as usize] ^= true;
+            }
+            y += 1;
+        }
+        x += 1;
+    }
+
+    // Mark all multiples of squares as non-prime
+    let mut r = 5;
+    while r*r < limit32 {
+        if sieve[r as usize] {
+            let mut i = r*r;
+            while i < limit32 {
+                sieve[i as usize] = false;
+                i += r * r;
+            }
+        }
+        r += 1;
+    }
+
+    // Print primes using sieve[]
+    for i in 5..limit32 {
+        if sieve[i as usize] {
+            res.push(i as u32);
+        }
+    }
+
+    return res;
+}
