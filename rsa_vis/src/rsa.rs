6da875c96c68e8c93f3b17379b67c12ac83b1df9
Ariel Young <arielyoung103@gmail.com>
2020-06-06 16:25:22 -0700

    Implemented (?) RSA encryption

--- rsa_vis/src/rsa.rs
+++ rsa_vis/src/rsa.rs
@@ -1,7 +1,59 @@
-// use num::BigInt;
-// use num::FromPrimitive;
+use num::{BigUint, Integer, ToPrimitive, FromPrimitive};
+use num::traits::One;
+use crate::rand;
+use crate::primes;
 
+const KEY_SIZE: usize = 1024;
+const BLOCK_SIZE: usize = 4;
 
-pub fn do_thing() {
-    println!("oh balls");
+fn _gen_key(num_prime_bits: usize) -> (BigUint, BigUint) {
+    let mut rng = rand::new();
+    let one: BigUint = One::one();
+
+    let prime_one = primes::gen_large_prime(num_prime_bits);
+    let prime_two = primes::gen_large_prime(num_prime_bits);
+
+    let prod: BigUint = (prime_one.clone() - one.clone()) * (prime_two.clone() - one.clone());
+    let mut exponent = rng.next_bigint(num_prime_bits/4);
+    // Lord forgive me
+    while exponent.divides(&prod.clone()) {
+        exponent = rng.next_bigint(num_prime_bits/4);
+        println!("Loop");
+    }
+
+    (&prime_one * &prime_two, exponent)
+}
+
+fn _encrypt_str(msg: &str, key: (BigUint, BigUint)) -> Vec<i32> {
+    let offset = if (msg.len() % BLOCK_SIZE) == 0 { 0 } else { 1 };
+
+    let mut blocks = vec![0_i32, (msg.len() / BLOCK_SIZE) as i32 + offset];
+    let mut block_index = 0;
+    let mut block_count = 0;
+    for c in msg.chars() {
+        blocks[block_index] += (c as i32) - 96;
+        block_count += 1;
+        if block_count == BLOCK_SIZE {
+            block_index += 1;
+            block_count = 0;
+        } else {
+            blocks[block_index] *= 10;
+        }
+    }
+
+    let mut output = vec![0_i32, (msg.len() / BLOCK_SIZE) as i32 + offset];
+    for (i, block) in blocks.iter().enumerate() {
+        output[i] = BigUint::from_i32(block.clone()).unwrap()
+            .modpow(&key.0.clone(), &key.1.clone())
+            .to_i32()
+            .unwrap();
+    }
+
+    output
+}
+
+pub fn test_thing() {
+    let num_prime_bits = 512;
+    let (n, e) = _gen_key(KEY_SIZE / 2);
+    println!("(n: {}, e: {})", n, e);
 }
