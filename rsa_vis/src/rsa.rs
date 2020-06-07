2ec35cef7709cf614f9444ddf00d838178ae2f0d
Ariel Young <arielyoung103@gmail.com>
2020-06-07 16:43:58 -0700

    Refactored functions in rsa.rs

--- rsa_vis/src/rsa.rs
+++ rsa_vis/src/rsa.rs
@@ -3,10 +3,9 @@ use num::traits::{One, Zero};
 use crate::rand;
 use crate::primes;
 use crate::num::bigint::ToBigInt;
-use std::borrow::Borrow;
 
 const KEY_SIZE: usize = 1024;
-const BLOCK_SIZE: usize = 4; // Block size in number of characters
+const BLOCK_SIZE: usize = 4; // Block size in increments of 8 bytes
 
 fn _gcd(a: BigUint, b: BigUint) -> BigUint {
     return if b == Zero::zero() {
@@ -75,7 +74,36 @@ fn _gen_key(num_prime_bits: usize) -> ((BigUint, BigUint), BigUint) {
     ((&prime_one * &prime_two, exponent), d)
 }
 
-fn _encrypt_str(msg: &str, key: (BigUint, BigUint)) -> Vec<BigUint> {
+fn _encrypt_bytes(blocks: Vec<u32>, key: (BigUint, BigUint)) -> Vec<BigUint> {
+    print!("Pre-Encrypt Blocks: [ ");
+    for b in &blocks { print!("{:#b}, ", b); }
+    println!("]");
+
+    let mut output: Vec<BigUint> = vec![BigUint::from_i32(0).unwrap();
+                                        blocks.len()];
+    for (i, block) in blocks.iter().enumerate() {
+        output[i] = BigUint::from_u32(block.clone()).unwrap()
+            .modpow(&key.1.clone(), &key.0.clone());
+    }
+
+    output
+}
+
+fn _decrypt_bytes(cipher: &Vec<BigUint>, privkey: BigUint, pubkey: BigUint) -> Vec<u32> {
+    let mut dec_blocks = vec![0_u32; cipher.len()];
+    for (i, enc_block) in cipher.iter().enumerate() {
+        dec_blocks[i] = enc_block.modpow(&privkey.clone(), &pubkey.clone())
+            .to_u32().unwrap();
+    }
+
+    print!("Post-Decrypt Blocks: [ ");
+    for b in &dec_blocks { print!("{:#b}, ", b); }
+    println!("]");
+
+    dec_blocks
+}
+
+fn _pack_string(msg: &str) -> Vec<u32> {
     let offset: usize = if (msg.len() % BLOCK_SIZE) == 0 { 0 } else { 1 };
 
     let mut blocks = vec![0_u32; (msg.len() / BLOCK_SIZE) + offset];
@@ -91,33 +119,13 @@ fn _encrypt_str(msg: &str, key: (BigUint, BigUint)) -> Vec<BigUint> {
             blocks[block_index] <<= 8;
         }
     }
-    print!("enc: [ ");
-    for b in &blocks { print!("{:#b}, ", b); }
-    println!("]");
 
-    let mut output: Vec<BigUint> = vec![BigUint::from_i32(0).unwrap();
-                                        (msg.len() / BLOCK_SIZE) + offset];
-    for (i, block) in blocks.iter().enumerate() {
-        output[i] = BigUint::from_u32(block.clone()).unwrap()
-            .modpow(&key.1.clone(), &key.0.clone());
-    }
-
-    output
+    blocks
 }
 
-fn _decrypt_str(cipher: Vec<BigUint>, privkey: BigUint, pubkey: BigUint) -> String {
-    let mut dec_blocks = vec![0_u32; cipher.len()];
-    for (i, enc_block) in cipher.iter().enumerate() {
-        dec_blocks[i] = enc_block.modpow(&privkey.clone(), &pubkey.clone())
-            .to_u32().unwrap();
-    }
-
-    print!("dec: [ ");
-    for b in &dec_blocks { print!("{:#b}, ", b); }
-    println!("]");
-
-    let mut res: String = String::with_capacity(dec_blocks.len() * BLOCK_SIZE);
-    for mut c in dec_blocks.iter_mut().rev() {
+fn _unpack_string(mut blocks: Vec<u32>) -> String {
+    let mut res: String = String::new();
+    for mut c in blocks.iter_mut().rev() {
         let mut c = *c;
         while c > 0 {
             let char = (c & 0b11111111) as u8;
@@ -131,10 +139,20 @@ fn _decrypt_str(cipher: Vec<BigUint>, privkey: BigUint, pubkey: BigUint) -> Stri
     res
 }
 
+fn _encrypt_str(msg: &str, key: (BigUint, BigUint)) -> Vec<BigUint> {
+    let packed_string = _pack_string(msg);
+    _encrypt_bytes(packed_string, key)
+}
+
+fn _decrypt_str(cipher: &Vec<BigUint>, privkey: BigUint, pubkey: BigUint) -> String {
+    let dec_blocks = _decrypt_bytes(cipher, privkey, pubkey);
+    _unpack_string(dec_blocks)
+}
+
 pub fn test_thing() {
     let (pubkey, privkey) = _gen_key(KEY_SIZE / 2);
     let cipher = _encrypt_str(&"Hello world, how are you today?".to_string(), pubkey.clone());
-    let dec_result = _decrypt_str(cipher, privkey, pubkey.0);
+    let dec_result = _decrypt_str(&cipher, privkey, pubkey.0);
 
     println!("Result: {}", dec_result);
 }
