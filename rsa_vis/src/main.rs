8694109a7a61821649774bcae462872aa2c35f85
Nashir Janmohamed <nashirbass@gmail.com>
2020-06-08 10:31:25 -0700

    Add debugging stuff to main for Ariel to look at

--- rsa_vis/src/main.rs
+++ rsa_vis/src/main.rs
@@ -22,8 +22,15 @@ fn main() {
     // test_primes(640);
     // rsa::test_thing();
     cli::init_cli_interface();
-    // io::write_key_to_disk(rsa::gen_key());
-    let (pub_key, priv_key) = io::read_key_from_disk();
+    let (pub_key, priv_key) = rsa::gen_key();
+    io::write_key_to_disk(rsa::gen_key());
+    println!("n: {}", pub_key.0);
+    println!("e: {}", pub_key.1);
+    println!("d: {}", priv_key);
+    // let (pub_key, priv_key) = io::read_key_from_disk();
+    // println!("n: {}", pub_key.0);
+    // println!("e: {}", pub_key.1);
+    // println!("d: {}", priv_key);
     // println!("{}\n{}\n{}", pub_key.0, pub_key.1, priv_key);
     io::encrypt_file("src.txt", "encrypted.txt", pub_key.clone());
     io::decrypt_file("encrypted.txt", "unencrypted.txt", priv_key.clone(), pub_key.clone().1);
