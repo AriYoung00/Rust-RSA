4bc8b44e213049d75423376872b731b25a668e16
Ariel Young <arielyoung103@gmail.com>
2020-06-08 00:58:59 -0700

    Implemented most of CLI interface

--- rsa_vis/src/cli.rs
+++ rsa_vis/src/cli.rs
@@ -1,7 +1,8 @@
 use std::io;
 use num::BigUint;
 
-use crate::rsa;
+use crate::{rsa, io as asdf};
+use std::error::Error;
 
 fn print_help() {
     println!("Key generation: ");
@@ -10,12 +11,13 @@ fn print_help() {
     println!("\t rk <filename> -> read key from <filename>");
 
     println!("Encryption: ");
-    println!("\t e <message> [filename] -> Encrypt message using stored key, optionally write resulting \
-    \n\t\t\tcipher to [filename]. If no filename is provided, cipher will be written to stdout and stored in memory");
+    println!("\t e <message> -> Encrypt message using stored key, storing cipher in memory.");
+    println!("\t wc <filename> -> Write stored cipher to <filename>");
+    println!("\t pc -> Prints stored cipher to stdout. Warning: very long line");
 
     println!("Decryption: ");
     println!("\t d -> Decrypt cipher stored in memory, display result to stdout");
-    println!("\t df <filename> -> Read cipher from file and decrypt, display result to stdout");
+    println!("\t df <filename> -> Read cipher from file and decrypt using stored key, display result to stdout");
 
     println!("Misc: ");
     println!("\t q -> Quit.");
@@ -26,33 +28,103 @@ fn print_help() {
 
 pub fn init_cli_interface() {
     println!("Rust implementation of RSA-1024, written by Ariel Young and Nashir Janmohamed\n");
-    println!("Commands are as follows:");
+    println!("Commands are as follows -- ");
     print_help();
-    print!("\n> ");
 
     let mut stored_key: Option<((BigUint, BigUint), BigUint)> = None;
+    let mut stored_cipher: Option<Vec<BigUint>> = None;
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Unable to read input");
-    let mut parts: Vec<&str> = input.split_ascii_whitespace().collect();
-    loop {
+
+    'outer: loop {
+        println!();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("> Unable to read input");
+        let mut parts: Vec<&str> = input.split_ascii_whitespace().collect();
+
         match parts[0] {
             "k" => {
-                println!("Generating key...");
+                println!("> Generating key...");
                 stored_key = Some(rsa::gen_key());
-                println!("Finished!");
+                println!("> Finished!");
             },
-            "wk" => unimplemented!("Hello yes this is unimplemented"),
-            "rk" => unimplemented!("Hello yes this is unimplemented as well"),
+            "wk" => match stored_key.clone() {
+                Some(key) => {asdf::write_key_to_disk(key); println!("> Done!");},
+                None => println!("> Error: No stored key")
+            },
+            "rk" => match asdf::read_key_from_disk() {
+                Ok(key) => {stored_key = Some(key); println!("> Done!");},
+                Err(e) => println!("> Error reading key from file: {}", e.to_string()),
+            },
 
             "e" => {
-                if parts.len() == 2 {
-                    println!("Encryption message...");
-                    let res = rsa::
+                if stored_key.is_none() {
+                    println!("> Error: No stored key");
+                    println!("> Either generate one, or read from file using 'rk <filename>'");
+                    continue 'outer;
+                }
+                if parts.len() >= 2 {
+                    println!("> Encrypting message...");
+                    let key = stored_key.clone().unwrap();
+                    let res = rsa::encrypt_str(&parts[1..].join(" "), key.0);
+                    println!("> Finished!");
+                    println!("> Result: {:?}", res);
+                    stored_cipher = Some(res);
+                } else {
+                    println!("> Error: invalid parameters to 'e'");
                 }
              },
 
-            _ => println!("Input not recognized, enter 'h' for help"),
+            "wc" => unimplemented!("> Writing cipher to file not yet implemented"),
+
+            "pc" => match stored_cipher.clone() {
+                Some(t) => {
+                    println!("> Stored cipher: {:?}", t);
+                    print!("> Hex: ");
+                    for b in &t { print!("{:x}", b); }
+                    println!();
+                },
+                None => println!("> Error: No stored cipher"),
+            }
+
+            "d" => {
+                if stored_cipher.is_none() {
+                    println!("> Error: No stored cipher.");
+                    println!("> Either encrypt a message using 'e', or decrypt a cipher from file using 'df'");
+                } else if stored_key.is_none() {
+                    println!("> Error: No stored key");
+                    println!("> You probably want to read one from disk using 'rk'");
+                } else {
+                    let key = stored_key.clone().unwrap();
+                    println!("> Decryption result: {}", rsa::decrypt_str(&stored_cipher.clone().unwrap(),
+                                                                       key.1, (key.0).0));
+                }
+
+
+            },
+
+            "df" => unimplemented!("> Decrypting cipher from file not yet implemented"),
+
+            "s" => {
+                if stored_key.is_none() {
+                    println!("> Key stored in memory: no");
+                } else {
+                    println!("> Key stored in memory: yes");
+                }
+                if stored_cipher.is_none() {
+                    println!("> Key stored in memory: no");
+                } else {
+                    println!("> Key stored in memory: yes");
+                }
+            },
+
+            "h" => print_help(),
+
+            "q" => {
+                println!("> Exiting...");
+                return;
+            },
+
+            _ => println!("> Input not recognized, enter 'h' for help"),
         }
     }
 }
